fab21d48c8f98d7305d5f56887997996c581fb48
v1n3k0 <v1n1c1u5_1@hotmail.com>
2016-10-21 23:40:56 -0200

    atualização index

--- index.php
+++ index.php
@@ -1,43 +1,51 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         
-        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen" />
         <title>Unifunda</title>
     </head>
     <body>
-        <div align="center">
-        <h3>Login</h3>
-       <form class="form-horizontal">
-		  <div class="form-group">
-		    <label for="inputEmail3" class="col-sm-4 control-label">Email</label>
-		    <div class="col-sm-4">
-		      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-		    </div>
-		  </div>
-		  <div class="form-group">
-		    <label for="inputPassword3" class="col-sm-4 control-label">Password</label>
-		    <div class="col-sm-4">
-		      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-		    </div>
-		  </div>
-		  <div class="form-group">
-		    <div class="col-sm-offset-2 col-sm-8">
-		      <div class="checkbox">
-		        <label>
-		          <input type="checkbox"> Remember me
-		        </label>
-		      </div>
-		    </div>
-		  </div>
-		  <div class="form-group">
-		    <div class="col-sm-offset-2 col-sm-8">
-		      <button type="submit" class="btn btn-default">Sign in</button>
-		    </div>
-		  </div>
-		</form>
-        </div>        
-        <script src="js/bootstrap.min.js"></script>
+    	<script src="js/bootstrap.js"></script>
+
+    	<div align="center" class="row">
+    		<div class="col-md-1 col-md-offset-4">
+    			<img src="image/LogoEFEI.png" alt="Logo Unifei" class="img-responsive">
+    		</div>
+    		<div class="col-md-2">
+    			<h2 class="text-center">UNIFunda <br> <small>UNIFEI</small></h2>
+    		</div>
+    	</div>
+
+    	<div class="row col-md-4 col-md-offset-4">
+	        <div align="center" class="panel panel-primary">
+		        <div class="panel-heading">
+		        	<h3 class="panel-title"> Entrar no UNIFunda </h3>
+		        </div>	        
+		        <div class="panel-body">
+			        <form class="form-horizontal">
+					  <div class="form-group">
+					    <label for="inputEmail3" class="col-md-2 col-md-offset-1 control-label">Login</label>
+					    <div class="col-md-7">
+					      <input type="text" class="form-control" id="exampleInputName" placeholder="Nickname">
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <label for="inputPassword3" class="col-md-2 col-md-offset-1 control-label">Senha</label>
+					    <div class="col-md-7">
+					      <input type="password" class="form-control" id="inputPassword3" placeholder="Senha">
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <div class="col-md-offset-5 col-md-1">
+					      <button type="submit" class="btn btn-default">Entrar</button>
+					    </div>
+					  </div>
+					</form>
+		        </div>
+	        </div>
+    	</div>
+
     </body>
 </html>
