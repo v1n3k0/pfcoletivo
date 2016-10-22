48dcecf09ab55eae67fa8aefb7b42c9bc7e80d09
v1n3k0 <v1n1c1u5_1@hotmail.com>
2016-10-22 10:39:22 -0200

    atualização

---
+++ header.php
@@ -0,0 +1,36 @@
+<?php include_once("conexao.php") ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UNIFunda</title>
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+	<div class="col-md-10 col-md-offset-1">
+	<nav class="navbar navbar-default">
+		<ul class="nav navbar-nav">
+			<li>
+				<a href="index.php">HOME</a>
+			</li>
+			<li>
+				<a href="buscar_genero.php">Filmes por Gênero</a>
+			</li>
+			<li>
+				<a href="buscar.php">Buscar Filme</a>
+			</li>
+			<li>
+				<a href="loginUsuario.php">Cadastrar</a>
+			</li>
+		</ul>
+	</nav>
+
+
+
+	
+
