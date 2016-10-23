a80812589a4224c63154d013cc4c65613a48c84d
v1n3k0 <v1n1c1u5_1@hotmail.com>
2016-10-23 09:58:22 -0200

    Ultimo commit

--- buscaUsuario.php
+++ buscaUsuario.php
@@ -15,7 +15,8 @@ if(isset($_GET['busca']))
 }
 
 ?>
-	 <div class="col-md-8 col-md-offset-2">    	
+<div class="row">
+	 <div class="row col-md-8 col-md-offset-2">    	
 	   	<div class="panel panel-primary">
 			<div class="panel-heading">
 					<h3 class="panel-title">Consultar Usuário</h3>
@@ -23,56 +24,58 @@ if(isset($_GET['busca']))
 			<div class="panel-body">
 			<form class="form-horizontal" method="GET" action="buscaUsuario.php" >
 					<div class="form-group">
-					    <label class="col-md-4 control-label">Busca</label>
+					    <label class="col-md-1 control-label">Busca</label>
 						<div class="col-md-8">
 						   	<input type="text" class="form-control" name="busca" placeholder="Palavra-chave">
 						</div>
+						<div class="col-md-3">
+							<button type="submit" class="btn btn-default">Consultar</button>
+						</div>
 					</div>
-					<div class="form-group">
-							    <div class="col-md-1 col-md-offset-9">
-							    	<button type="submit" class="btn btn-default">Consultar</button>
-							    </div>
-							</div>
-					</form>
-				</div>
+			</form>
 			</div>
 		</div>
-<div class="col-md-12" style="margin-top: 50px">
-	<?php 
-	$aux =0;
-	if(isset($result))
-	{		
-		if(mysqli_num_rows($result) > 0)
-		{
-		
-			?><table class="table table-hover">
-					<tr>
-						<td><b>Login</b></td>
-						<td><b>Nome</b></td>
-					</tr>
-			<?php
-			while($usuario = mysqli_fetch_object($result))
-			{
-				?>
-				<div class="row" style="margin-bottom: 20px;">
-				<div class="col-md-10">
-					<tr>
-						<td><span class="detalhes"><a href="dadosUsuario.php?cpf=<?php echo $usuario->cpf; ?>"><?php echo $usuario->login ?></a></span><br></td>
-						<td><span class="detalhes"><a href="dadosUsuario.php?cpf=<?php echo $usuario->cpf; ?>"><?php echo $usuario->nome ?></a></span><br>
-					</td>
-					</tr>
-				</div>
-				</div>
+	</div>
+</div>
+<div class="row ">
+	<div class="col-md-10 col-md-offset-1">
+			<?php 
+			$aux =0;
+			if(isset($result))
+			{		
+				if(mysqli_num_rows($result) > 0)
+				{
 				
-				<?php
+					?>
+					
+					<div class="panel panel-primary">
+		  			<div class="panel-heading">Resultado</div>
+					<table class="table table-striped">
+							<tr>
+								<td><b>Login</b></td>
+								<td><b>Nome</b></td>
+							</tr>
+					<?php
+					while($usuario = mysqli_fetch_object($result))
+					{
+						?>
+						<tr>
+							<td><span class="detalhes"><a href="dadosUsuario.php?cpf=<?php echo $usuario->cpf; ?>"><?php echo $usuario->login ?></a></span><br></td>
+							<td><span class="detalhes"><a href="dadosUsuario.php?cpf=<?php echo $usuario->cpf; ?>"><?php echo $usuario->nome ?></a></span><br>
+							</td>
+						</tr>
+						
+						<?php
+					}
+					?></table> <?php	
+				}else
+				{		
+					echo "Nenhum usuario encontrado<b></b>";
+				}
 			}
-			?></table> <?php	
-		}else
-		{		
-			echo "Nenhum usuario encontrado<b></b>";
-		}
-	}
-	?>
+			?>
+		</div>
+	</div>
 </div>
 
 
--- dadosUsuario.php
+++ dadosUsuario.php
@@ -10,83 +10,54 @@ $result = mysqli_query($con, "SELECT * FROM usuario WHERE cpf = '$cpf'");
 
 ?>
 	 
-<div class="col-md-12" style="margin-top: 50px">
-	<?php 
-	$aux =0;
-	if(isset($result))
-	{		
-		if(mysqli_num_rows($result) > 0)
-		{
-		
-			?><table class="table table-hover">
-					<tr>
-						<td><b>Login</b></td>
-						<td><b>Nome</b></td>
-						<td><b>Pais</b></td>
-						<td><b>Cidade</b></td>
-						<td><b>Estado</b></td>
-						<td><b>Data de nascimento</b></td>
-						<td><b>E-mail</b></td>
-						<td><b>Tipo</b></td>
-						<td><b>Categoria</b></td>
-
-					</tr>
-			<?php
-			while($usuario = mysqli_fetch_object($result))
+<div class="row col-md-12">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Dados do usuario</h3>
+		</div>
+		<?php 
+		$aux =0;
+		if(isset($result))
+		{		
+			if(mysqli_num_rows($result) > 0)
 			{
-				?>
-				<div class="row" style="margin-bottom: 20px;">
-				<div class="col-md-10">
-					
-						<td><span class="detalhes"><?php echo $usuario->login ?></a></span><br></td>
-						<td><span class="detalhes"><?php echo $usuario->nome ?></a></span><br></td>
-						<td><span class="detalhes"><?php echo $usuario->pais ?></a></span><br></td>
-						<td><span class="detalhes"><?php echo $usuario->cidade ?></a></span><br></td>
-						<td><span class="detalhes"><?php echo $usuario->estado ?></a></span><br></td>
-						<td><span class="detalhes"><?php echo $usuario->data_n ?></a></span><br></td>
-						<td><span class="detalhes"><?php echo $usuario->email ?></a></span><br></td>
-						<td><span class="detalhes"><?php echo $usuario->tipo ?></a></span><br></td>
-						<td><span class="detalhes"><?php echo $usuario->categoria ?></a></span><br></td>
-						
-				</div>
-				</div>
-				
-				<?php
-			}
-			?></table> <?php	
-		}
-		elseif (isset($result1)) {
-			if(mysqli_num_rows($result1) > 0)
-				$aux = 1;
-			{
-				?><table class="table table-hover">
+			
+				?><table class="table table-striped">
 						<tr>
 							<td><b>Login</b></td>
 							<td><b>Nome</b></td>
+							<td><b>Pais</b></td>
+							<td><b>Cidade</b></td>
+							<td><b>Estado</b></td>
+							<td><b>Data de nascimento</b></td>
+							<td><b>E-mail</b></td>
+							<td><b>Tipo</b></td>
+							<td><b>Categoria</b></td>
+
 						</tr>
 				<?php
-				while($usuario = mysqli_fetch_object($result1))
+				while($usuario = mysqli_fetch_object($result))
 				{
-					?>
-					<div class="row" style="margin-bottom: 20px;">
-					<div class="col-md-10">
-						
-							<td><span class="detalhes"><a href="dadosUsuario.php?cpf='$usuario->cpf'"><?php echo $usuario->login ?></a></span><br></td>
-							<td><span class="detalhes"><a href="dadosUsuario.php?cpf='$usuario->cpf'"><?php echo $usuario->nome ?></a></span><br>
-						</td>
-					</div>
-					</div>
-					
-					<?php
+				?>					
+					<td><span class="detalhes"><?php echo $usuario->login ?></a></span><br></td>
+					<td><span class="detalhes"><?php echo $usuario->nome ?></a></span><br></td>
+					<td><span class="detalhes"><?php echo $usuario->pais ?></a></span><br></td>
+					<td><span class="detalhes"><?php echo $usuario->cidade ?></a></span><br></td>
+					<td><span class="detalhes"><?php echo $usuario->estado ?></a></span><br></td>
+					<td><span class="detalhes"><?php echo $usuario->data_n ?></a></span><br></td>
+					<td><span class="detalhes"><?php echo $usuario->email ?></a></span><br></td>
+					<td><span class="detalhes"><?php echo $usuario->tipo ?></a></span><br></td>
+					<td><span class="detalhes"><?php echo $usuario->categoria ?></a></span><br></td>
+				<?php
 				}
 				?></table> <?php	
+			}elseif($aux = 0)
+			{		
+				echo "Nenhum usuario encontrado<b></b>";
 			}
-		}elseif($aux = 0)
-		{		
-			echo "Nenhum usuario encontrado<b></b>";
 		}
-	}
-	?>
+		?>
+	</div>
 </div>
 
 
