d52f075f852455a57f03a701cf72b25730dba997
v1n3k0 <v1n1c1u5_1@hotmail.com>
2016-10-23 09:16:41 -0200

    Update front end alterar usuario

--- alterarUsuario.php
+++ alterarUsuario.php
@@ -9,103 +9,113 @@ $result = mysqli_query($con, "SELECT * FROM usuario WHERE cpf = '$cpf'");
  
 
 ?>
-	 
-<div class="col-md-12" style="margin-top: 50px">
 	<?php 
 	$aux =0;
 	if(isset($result))
 	{		
 		if(mysqli_num_rows($result) > 0)
 		{
-			
 			?>
-			<div class="row">
-			<h3><b>Dados pessoais</b></h3>
-			<table class="table table-hover">
-			<?php
-			while($usuario = mysqli_fetch_object($result))
-			{
-				?>
-				<div class="row" style="margin-bottom: 20px;">
-				<div class="col-md-10">
-					<form class="form-horizontal" method="POST" action="updateUsuario.php" >							
-						<tr>
-							<td><span class="detalhes"><?php echo $usuario->login ?></a></span><br></td>
-							<td><input type="text" class="form-control" name="login" placeholder="Login" size="4"></td>
-							<td><button type="submit" class="btn btn-default">Atualizar</button></td>
-						</tr>
-						<tr>
-							<td><span class="detalhes"><?php echo "Senha" ?></a></span><br></td>
-							<td><input type="password" class="form-control" name="senha" placeholder="Senha" size="4"></td>
-							<td><button type="submit" class="btn btn-default">Atualizar</button></td>
-						</tr>
-						<tr>
-							<td><span class="detalhes"><?php echo " Confirmar Senha" ?></a></span><br></td>
-							<td><input type="password" class="form-control" name="repetirSenha" placeholder="Confirmar Senha" size="4"></td>
-							<td><button type="submit" class="btn btn-default">Atualizar</button></td>
-						</tr>
-						<tr>
-							<td><span class="detalhes"><?php echo $usuario->pais ?></a></span><br></td>
-							<td><input type="text" class="form-control" name="pais" placeholder="Brasil" size="4"></td>
-							<td><button type="submit" class="btn btn-default">Atualizar</button></td></tr>
-						<tr>
-							<td><span class="detalhes"><?php echo $usuario->cidade ?></a></span><br></td>
-							<td><input type="text" class="form-control" name="cidade" placeholder="Itajuba" size="4"></td>
-							<td><button type="submit" class="btn btn-default">Atualizar</button></td>
-						</tr>
-						<tr>		
-							<td><span class="detalhes"><?php echo $usuario->estado ?></a></span><br></td>
-							<td>
-							<select class="form-control" name="estado">
-									<option value="ac">Acre</option> 
-									<option value="al">Alagoas</option> 
-									<option value="am">Amazonas</option> 
-									<option value="ap">Amapá</option> 
-									<option value="ba">Bahia</option> 
-									<option value="ce">Ceará</option> 
-									<option value="df">Distrito Federal</option> 
-									<option value="es">Espírito Santo</option> 
-									<option value="go">Goiás</option> 
-									<option value="ma">Maranhão</option> 
-									<option value="mt">Mato Grosso</option> 
-									<option value="ms">Mato Grosso do Sul</option> 
-									<option value="mg">Minas Gerais</option> 
-									<option value="pa">Pará</option> 
-									<option value="pb">Paraíba</option> 
-									<option value="pr">Paraná</option> 
-									<option value="pe">Pernambuco</option> 
-									<option value="pi">Piauí</option> 
-									<option value="rj">Rio de Janeiro</option> 
-									<option value="rn">Rio Grande do Norte</option> 
-									<option value="ro">Rondônia</option> 
-									<option value="rs">Rio Grande do Sul</option> 
-									<option value="rr">Roraima</option> 
-									<option value="sc">Santa Catarina</option> 
-									<option value="se">Sergipe</option> 
-									<option value="sp">São Paulo</option> 
-									<option value="to">Tocantins</option> 
-							</select>
-						</td>
-							<td><button type="submit" class="btn btn-default">Atualizar</button></td>
-						</tr>
-						
-						<tr>		
-							<td><span class="detalhes"><?php echo $usuario->email ?></a></span><br></td>
-							<td><input type="email" class="form-control" name="email" placeholder="email@email.com" size="4"></td>
-							<td><button type="submit" class="btn btn-default">Atualizar</button></td>
-						</tr>
-					</form>
-				</div>
-				</div>
-					
+			<div class="col-md-10 col-md-offset-1">
 				<?php
-			}
-			?></table> <?php	
+				if($usuario = mysqli_fetch_object($result))
+				{
+					?>
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Dados pessoais</h3>				
+					</div>
+					<div class="panel-body">
+						<form class="form-horizontal" method="POST" action="updateUsuario.php" >
+							<div class="form-group">
+								<label class="col-md-3 control-label">Login</label>
+								<div class="col-md-8">
+									<input type="text" class="form-control" name="login" placeholder=<?php echo $usuario->login ?>>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label">Senha</label>
+								<div class="col-md-8">
+									<input type="password" class="form-control" name="senha" placeholder="Senha">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label">Confirmar Senha</label>
+								<div class="col-md-8">
+									<input type="password" class="form-control" name="repetirSenha" placeholder="Confirmar Senha">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label">Pais</label>
+								<div class="col-md-8">
+									<input type="text" class="form-control" name="pais" placeholder=<?php echo $usuario->pais ?>>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label">Cidade</label>
+								<div class="col-md-8">
+									<input type="text" class="form-control" name="cidade" placeholder=<?php echo $usuario->cidade ?>>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label class="col-md-3 control-label">Estado</label>
+								<div class="col-md-8">
+									<select class="form-control" name="estado">
+										<option value=<?php echo $usuario->estado ?>><?php echo $usuario->estado ?></option> 
+										<option value="ac">Acre</option> 
+										<option value="al">Alagoas</option> 
+										<option value="am">Amazonas</option> 
+										<option value="ap">Amapá</option> 
+										<option value="ba">Bahia</option> 
+										<option value="ce">Ceará</option> 
+										<option value="df">Distrito Federal</option> 
+										<option value="es">Espírito Santo</option> 
+										<option value="go">Goiás</option> 
+										<option value="ma">Maranhão</option> 
+										<option value="mt">Mato Grosso</option> 
+										<option value="ms">Mato Grosso do Sul</option> 
+										<option value="mg">Minas Gerais</option> 
+										<option value="pa">Pará</option> 
+										<option value="pb">Paraíba</option> 
+										<option value="pr">Paraná</option> 
+										<option value="pe">Pernambuco</option> 
+										<option value="pi">Piauí</option> 
+										<option value="rj">Rio de Janeiro</option> 
+										<option value="rn">Rio Grande do Norte</option> 
+										<option value="ro">Rondônia</option> 
+										<option value="rs">Rio Grande do Sul</option> 
+										<option value="rr">Roraima</option> 
+										<option value="sc">Santa Catarina</option> 
+										<option value="se">Sergipe</option> 
+										<option value="sp">São Paulo</option> 
+										<option value="to">Tocantins</option> 
+									</select>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label">Estado</label>
+								<div class="col-md-8">
+									<input type="email" class="form-control" name="email" placeholder=<?php echo $usuario->email ?>>
+								</div>
+							</div>
+							<div class="form-group">
+					    		<div class="col-md-1 col-md-offset-9">
+									<button type="submit" class="btn btn-default">Atualizar</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			
+				<?php
+				}
+				?>
+			</div>
+			<?php	
 		}
 		
 	}
 	?>
-</div>
-
 
 <?php include_once("footer.php") ?>
